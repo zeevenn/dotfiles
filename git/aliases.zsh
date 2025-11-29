7532b6bf53df4c6504a65478267cf7420d676ee4
ziven <contact@ziven.me>
2025-11-29 14:11:32 +0800

    Add main branch to git-wtf integration branches

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,24 +1,26 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
-
 # The rest of my fun git aliases
+alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gf='git fetch'
 alias gp='git push origin HEAD'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
+alias ga='git add'
 alias gc='git commit'
 alias gca='git commit -a'
+alias gcam='git commit -a -m'
 alias gco='git checkout'
-alias gcb='git copy-branch-name'
+alias gcp='git cherry-pick'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
+alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias ge='git-edit-new'
+alias gsw='git switch'
+alias gm='git merge'
+alias gr='git rebase'
+alias gsh='git stash'
+alias gshp='git stash pop'
+alias gshl='git stash list'
+alias gsha='git stash apply'
