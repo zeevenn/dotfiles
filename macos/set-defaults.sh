fef850779a8aceceb776dd05687ed4602ff5df58
ziven <contact@ziven.me>
2025-11-30 10:33:04 +0800

    Remove kill

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -115,28 +115,10 @@ defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 # Activity Monitor                                                            #
 ###############################################################################
 
-# Show the main window when launching Activity Monitor
-defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
-
 # Visualize CPU usage in the Activity Monitor Dock icon
 defaults write com.apple.ActivityMonitor IconType -int 5
 
 # Show all processes in Activity Monitor
 defaults write com.apple.ActivityMonitor ShowCategory -int 0
 
-# Sort Activity Monitor results by CPU usage
-defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
-defaults write com.apple.ActivityMonitor SortDirection -int 0
-
-###############################################################################
-# Kill affected applications                                                  #
-###############################################################################
-
-for app in "Activity Monitor" \
-	"Dock" \
-	"Finder" \
-	"Safari" \
-	"SystemUIServer"; do
-	killall "${app}" &> /dev/null
-done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
