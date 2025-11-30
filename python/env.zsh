d045a897569384ead4abfec4e46aa2e74bd6f57e
ziven <contact@ziven.me>
2025-11-30 15:37:33 +0800

    Update py config

--- python/env.zsh
+++ python/env.zsh
@@ -8,8 +8,3 @@ export UV_CACHE_DIR="$HOME/.cache/uv"
 
 # Python virtual environment settings
 export VIRTUAL_ENV_DISABLE_PROMPT=1
-
-# pip configuration
-export PIP_REQUIRE_VIRTUALENV=false
-export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-
