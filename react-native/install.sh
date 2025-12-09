cbf6e9031fccfc750e7587f191625ecf9aae4ba8
ziven <contact@ziven.me>
2025-12-09 22:21:13 +0800

    Install ruby via rbenv

--- react-native/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Install React Native iOS dependencies
-
-# CocoaPods - iOS dependency manager
-if ! command -v pod &> /dev/null; then
-  echo "  Installing CocoaPods..."
-  gem install cocoapods
-fi
