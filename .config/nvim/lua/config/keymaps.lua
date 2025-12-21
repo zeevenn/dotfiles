60c21d4251585f7c2046b446a22d012b46e935d5
ziven <contact@ziven.me>
2025-12-21 21:03:46 +0800

    Add keymap for terminal switch in VScode style

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,8 @@ local keymap = vim.keymap
 
 keymap.set("n", "+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "-", "<C-x>", { desc = "Decrement number" })
+
+-- VScode style
+keymap.set({ "n", "t" }, "<C-`>", function()
+  Snacks.terminal()
+end, { desc = "Switch to terminal" })
