673df15fdf371b35ac4f4a65c18d83e0fcc4a90a
ziven <contact@ziven.me>
2025-12-25 12:34:15 +0800

    scrollEOF

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -66,4 +66,16 @@ return {
     "sindrets/diffview.nvim",
     cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFileHistory" },
   },
+
+  -- Keep scrolloff at the end of file
+  -- {
+  --   "Aasim-A/scrollEOF.nvim",
+  --   event = { "CursorMoved", "WinScrolled" },
+  --   opts = {
+  --     insert_mode = true,
+  --     disabled_filetypes = {
+  --       "snacks_terminal", -- Fix flickering in LazyGit and terminals
+  --     },
+  --   },
+  -- },
 }
