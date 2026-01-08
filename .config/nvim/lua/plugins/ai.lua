2297d7e3b1db60f75354b51d73a9367911b04dff
ziven <contact@ziven.me>
2026-01-09 00:24:08 +0800

    Remove avante.nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,20 +1,5 @@
 return {
   {
-    "yetone/avante.nvim",
-    enabled = false,
-    opts = {
-      provider = "bedrock",
-      providers = {
-        bedrock = {
-          model = "anthropic.claude-sonnet-4-20250514-v1:0",
-          aws_region = "us-west-2",
-          aws_profile = "claude-profile",
-        },
-      },
-    },
-  },
-
-  {
     "github/copilot.vim",
   },
 
