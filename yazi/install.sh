250625b314dc07062c318ba267b224c2c432f149
ziven <contact@ziven.me>
2026-02-13 21:37:57 +0800

    Update yazi insatll script

--- yazi/install.sh
+++ yazi/install.sh
@@ -4,15 +4,13 @@
 
 set -e
 
-if ! command -v ya &> /dev/null; then
+if ! command -v ya &>/dev/null; then
   echo "yazi not installed, skipping yazi setup"
   exit 0
 fi
 
 echo "Installing yazi plugins and themes..."
 
-# Themes
-ya pkg add yazi-rs/flavors:catppuccin-mocha
+ya pkg install
 
 echo "yazi setup complete!"
-
