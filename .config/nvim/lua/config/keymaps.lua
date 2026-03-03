4027cf4fe85f28292ade9aa7682f8c49cdece437
ziven <contact@ziven.me>
2026-03-03 19:14:44 +0800

    Remove C-` keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,11 +11,6 @@ keymap.set("n", "-", "<C-x>", { desc = "Decrement number" })
 -- https://www.reddit.com/r/neovim/comments/1mcvzmi/lazyvim_space_key_occasionally_acts_as_regular/
 keymap.set("n", " ", "<Nop>", { silent = true })
 
--- VScode style
-keymap.set({ "n", "t" }, "<C-`>", function()
-  Snacks.terminal()
-end, { desc = "Switch to terminal" })
-
 -- git open
 keymap.set("n", "<leader>go", function()
   vim.cmd("!git open")
