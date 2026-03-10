caf27e4da7a1beff9324382fa99371abd6b417fd
ziven <contact@ziven.me>
2026-03-10 11:34:34 +0800

    Add live-server

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -186,4 +186,26 @@ return {
       })
     end,
   },
+
+  {
+    "selimacerbas/live-server.nvim",
+    opts = {
+      default_port = 8000,
+      live_reload = { enabled = true, inject_script = true, debounce = 120, css_inject = true },
+      directory_listing = { enabled = true, show_hidden = false },
+    },
+    -- map to user commands (robust lazy-loading)
+    keys = {
+      { "<leader>ls", "<cmd>LiveServerStart<cr>", desc = "Start (pick path & port)" },
+      { "<leader>lo", "<cmd>LiveServerOpen<cr>", desc = "Open existing port in browser" },
+      { "<leader>lr", "<cmd>LiveServerReload<cr>", desc = "Force reload (pick port)" },
+      { "<leader>lt", "<cmd>LiveServerToggleLive<cr>", desc = "Toggle live-reload (pick port)" },
+      { "<leader>li", "<cmd>LiveServerStatus<cr>", desc = "Show server status" },
+      { "<leader>lS", "<cmd>LiveServerStop<cr>", desc = "Stop one (pick port)" },
+      { "<leader>lA", "<cmd>LiveServerStopAll<cr>", desc = "Stop all" },
+    },
+    config = function(_, opts)
+      require("live_server").setup(opts)
+    end,
+  },
 }
